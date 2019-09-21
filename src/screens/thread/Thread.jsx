d0909aa3449aecf830d16de541556b4dc63b5418
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-21 17:29:51 +0200

    nothing

---
+++ src/screens/thread/Thread.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+
+class Thread extends Component {
+  state = {}
+  render() {
+    return <div>Threads for the given channel!</div>
+  }
+}
+
+export default Thread
