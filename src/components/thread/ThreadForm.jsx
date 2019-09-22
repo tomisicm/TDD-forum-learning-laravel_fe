fbe6345ba72f04ec68f72a3acb02af18008eb372
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-22 15:30:41 +0200

    render depending on edit state

--- src/components/thread/ThreadForm.jsx
+++ src/components/thread/ThreadForm.jsx
@@ -5,26 +5,35 @@ import Form from "../../components/common/Form"
 
 class ThreadForm extends Form {
   state = {
-    edit: false
+    formEditState: false
   }
 
   toggleEditState() {
-    this.state.edit = !this.state.edit
-    console.log(this.state.edit)
+    this.setState({ formEditState: !this.state.formEditState })
   }
 
   handleDelete() {
     console.log(this.props.thread.id)
   }
 
+  handleSave() {
+    // async call
+    this.toggleEditState()
+  }
   render() {
     const { thread } = this.props
+    const { formEditState } = this.state
+
     return (
       <React.Fragment>
         <div className="card my-2" key={thread.id}>
           <div className="card-header">
             <div className="row justify-content">
-              <div className="col-md-8">Title: {thread.title}</div>
+              <div className="col-md-8">
+                {!formEditState && <span>Title: {thread.title}</span>}
+                {formEditState && <div>INSERT INPUT</div>}
+              </div>
+              {/* TODO: Remove current user is user */}
               {thread.creator && (
                 <div className="col">
                   <Link to={`/profiles/${thread.creator.name}`}>
@@ -35,7 +44,10 @@ class ThreadForm extends Form {
             </div>
           </div>
           <div className="card-body" style={{ padding: "0.5rem" }}>
-            <div className="card-text my-3">{thread.body}</div>
+            {!formEditState && (
+              <div className="card-text my-3">{thread.body}</div>
+            )}
+            {formEditState && <div>INSERT TEXTAREA</div>}
           </div>
           <div className="card-footer">
             <div className="row text-muted">
@@ -45,22 +57,36 @@ class ThreadForm extends Form {
               <div className="col mr-auto">
                 Last Update: {thread.updated_at}
               </div>
-              {/* TODO: Remove if user */}
+              {/* TODO: Remove if user not current user*/}
               <div className="col-auto">
-                <button
-                  onClick={() => this.toggleEditState()}
-                  type="button"
-                  className="btn btn-primary mx-2"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => this.handleDelete()}
-                  type="button"
-                  className="btn btn-danger mx-2"
-                >
-                  Delete
-                </button>
+                {!formEditState && (
+                  <React.Fragment>
+                    <button
+                      onClick={() => this.toggleEditState()}
+                      type="button"
+                      className="btn btn-primary mx-2"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => this.handleDelete()}
+                      type="button"
+                      className="btn btn-danger mx-2"
+                    >
+                      Delete
+                    </button>
+                  </React.Fragment>
+                )}
+
+                {formEditState && (
+                  <button
+                    onClick={() => this.handleSave()}
+                    type="button"
+                    className="btn btn-danger mx-2"
+                  >
+                    Save
+                  </button>
+                )}
               </div>
             </div>
           </div>
