1c260c743c5a8d67dbec67578d9b0fd6c72aefbc
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-27 11:51:31 +0200

    mistakes were made

--- src/redux/actions/channelAction.js
+++ src/redux/actions/channelAction.js
@@ -3,7 +3,7 @@ import channelsService from "../../utils/services/channels-service"
 
 export function fetchChannels() {
   return async function(dispatch, getState) {
-    const data = await channelsService.getChannels()
+    const { data } = await channelsService.getChannels()
 
     dispatch({ type: FETCH_CHANNELS, payload: data })
   }
