6e135835b265abef7da4a84e333b87606cf036a0
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-27 08:47:09 +0200

    nothing

--- src/components/common/BaseDropdownSelect.jsx
+++ src/components/common/BaseDropdownSelect.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const BaseDropdownSelect = () => {
   return (
-    <div className="btn-group dropup">
+    <div className="btn-group dropdown">
       <button
         type="button"
         className="btn btn-secondary dropdown-toggle"
@@ -12,7 +12,10 @@ const BaseDropdownSelect = () => {
       >
         Dropup
       </button>
-      <div className="dropdown-menu"></div>
+      <div className="dropdown-menu">
+        <div className="dropdown-item">Action</div>
+        <div className="dropdown-item">Action 2</div>
+      </div>
     </div>
   )
 }
