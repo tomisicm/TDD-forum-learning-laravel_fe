18883ea7852e78169a9f632fb4ded6b2a525e129
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-29 10:44:57 +0200

    agdf

--- src/components/thread/Reply.jsx
+++ src/components/thread/Reply.jsx
@@ -7,26 +7,33 @@ import BaseTextarea from "../../components/common/BaseTextarea"
 class Reply extends Component {
   state = {
     formEditState: false,
-    reply: {
-      body: "",
-      creator: {}
-    }
+    reply: null
   }
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.reply !== state.reply) {
-      return { reply: { ...props.reply } }
-    }
+  componentWillMount(prevProps) {
+    // if (this.props.reply !== prevProps.reply) {
+    this.setState({ reply: this.props.reply })
+    // }
   }
+
+  // static getDerivedStateFromProps(props, state) {
+  //   if (props.reply !== state.reply) {
+  //     return { reply: { ...props.reply } }
+  //   }
+  // }
 
   toggleEditState() {
     this.setState({ formEditState: !this.state.formEditState })
   }
 
   updateReply = ({ target }) => {
-    console.log(target.value)
+    const reply = {
+      ...this.state.reply,
+      body: target.value
+    }
+
     this.setState({
-      [target.name]: { body: target.value }
+      reply: reply
     })
   }
 
@@ -62,85 +69,86 @@ class Reply extends Component {
   }
 
   render() {
-    const { reply } = this.state
-    const { formEditState } = this.state
+    const { formEditState, reply } = this.state
 
     return (
       <React.Fragment>
-        <div className="card my-2" key={reply.id}>
-          <div className="card-header">
-            <div className="row justify-content">
-              <div className="col">
-                <Link to={`/profile/${reply.creator.name}`}>
-                  {reply.creator.name}
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="card-body" style={{ padding: "0.5rem" }}>
-            <div className="card-text">
-              {!formEditState && <div>{reply.body}</div>}
-              {formEditState && (
-                <div>
-                  <BaseTextarea
-                    value={reply.body}
-                    onChange={this.updateReply}
-                    name={"reply"}
-                    className="w-100"
-                  />
+        {reply && (
+          <div className="card my-2" key={reply.id}>
+            <div className="card-header">
+              <div className="row justify-content">
+                <div className="col">
+                  <Link to={`/profile/${reply.creator.name}`}>
+                    {reply.creator.name}
+                  </Link>
                 </div>
-              )}
+              </div>
             </div>
-          </div>
-
-          {(this.isDeletable() || this.isEditable()) && (
-            <div className="card-footer">
-              <div className="row text-muted">
-                {!formEditState && (
-                  <React.Fragment>
-                    {this.isEditable() && (
-                      <button
-                        onClick={() => this.toggleEditState()}
-                        type="button"
-                        className="btn-sm btn-primary mx-2"
-                      >
-                        Edit
-                      </button>
-                    )}
-                    {this.isDeletable() && (
-                      <button
-                        onClick={this.handleDelete}
-                        type="button"
-                        className="btn-sm btn-danger mx-2"
-                      >
-                        Delete
-                      </button>
-                    )}
-                  </React.Fragment>
-                )}
-
+            <div className="card-body" style={{ padding: "0.5rem" }}>
+              <div className="card-text">
+                {!formEditState && <div>{reply.body}</div>}
                 {formEditState && (
-                  <React.Fragment>
-                    <button
-                      onClick={() => this.handleSave()}
-                      type="button"
-                      className="btn-sm btn-success mx-2"
-                    >
-                      Save
-                    </button>
-                    <button
-                      onClick={() => this.handleCancel()}
-                      type="button"
-                      className="btn-sm btn-info mx-2"
-                    >
-                      Cancel
-                    </button>
-                  </React.Fragment>
+                  <div>
+                    <BaseTextarea
+                      value={reply.body}
+                      onChange={this.updateReply}
+                      name={"reply"}
+                      className="w-100"
+                    />
+                  </div>
                 )}
               </div>
             </div>
-          )}
-        </div>
+
+            {(this.isDeletable() || this.isEditable()) && (
+              <div className="card-footer">
+                <div className="row text-muted">
+                  {!formEditState && (
+                    <React.Fragment>
+                      {this.isEditable() && (
+                        <button
+                          onClick={() => this.toggleEditState()}
+                          type="button"
+                          className="btn-sm btn-primary mx-2"
+                        >
+                          Edit
+                        </button>
+                      )}
+                      {this.isDeletable() && (
+                        <button
+                          onClick={this.handleDelete}
+                          type="button"
+                          className="btn-sm btn-danger mx-2"
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </React.Fragment>
+                  )}
+
+                  {formEditState && (
+                    <React.Fragment>
+                      <button
+                        onClick={() => this.handleSave()}
+                        type="button"
+                        className="btn-sm btn-success mx-2"
+                      >
+                        Save
+                      </button>
+                      <button
+                        onClick={() => this.handleCancel()}
+                        type="button"
+                        className="btn-sm btn-info mx-2"
+                      >
+                        Cancel
+                      </button>
+                    </React.Fragment>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </React.Fragment>
     )
   }
